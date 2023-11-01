0d820da2bbe63169d7b0ab37a2d0af62a3bd34da
BAGAS <anndptra658@gmail.com>
2023-11-01 14:39:58 +0700

    build ignore

---
+++ app/src/main/java/com/digimbanking/Features/Auth/CreateRekening/Kartu/PilihKartu.kt
@@ -0,0 +1,12 @@
+package com.digimbanking.Features.Auth.CreateRekening.Kartu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.digimbanking.R
+
+class PilihKartu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pilih_kartu)
+    }
+}
