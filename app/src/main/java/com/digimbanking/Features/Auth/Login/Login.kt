bc04c0442181c69a359d3e65bba017c5d6960838
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2023-11-08 14:09:32 +0700

    add logic login

--- app/src/main/java/com/digimbanking/Features/Auth/Login/Login.kt
+++ app/src/main/java/com/digimbanking/Features/Auth/Login/Login.kt
@@ -2,6 +2,7 @@ package com.digimbanking.Features.Auth.Login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.ViewModelProvider
 import com.digimbanking.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,6 +18,26 @@ class Login : AppCompatActivity() {
         loginViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
 
         binding.apply {
+            tilLoginEmail.editText?.doOnTextChanged{ text, start, before, count ->
+                if(loginViewModel.validateEmail(text.toString())){
+                    binding.tilLoginEmail.isErrorEnabled = false
+                    binding.btnLoginMasuk.isEnabled = true
+                } else {
+                    binding.tilLoginEmail.isErrorEnabled = true
+                    binding.btnLoginMasuk.isEnabled = false
+                    binding.tilLoginEmail.error = "Email harus sesuai format penulisan"
+                }
+            }
+            tilLoginPw.editText?.doOnTextChanged { text, start, before, count ->
+                if(loginViewModel.validatePassword(text.toString())){
+                    binding.tilLoginPw.isErrorEnabled = false
+                    binding.btnLoginMasuk.isEnabled = true
+                } else {
+                    binding.tilLoginPw.isErrorEnabled = true
+                    binding.btnLoginMasuk.isEnabled = false
+                    binding.tilLoginPw.error = "Kata Sandi harus terdiri dari minimal 8 karakter"
+                }
+            }
         }
     }
 }
