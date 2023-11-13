02dd2135e9115777c5b80664481e31b2cb1574a3
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2023-11-13 14:51:57 +0700

    add splash versioning (masih belum bisa memunculkan nama versi)

--- app/src/main/java/com/digimbanking/Features/Auth/OnBoard/Splash.kt
+++ app/src/main/java/com/digimbanking/Features/Auth/OnBoard/Splash.kt
@@ -1,12 +1,30 @@
 package com.digimbanking.Features.Auth.OnBoard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.digimbanking.R
+import android.os.Handler
+import android.util.Log
+import com.digimbanking.databinding.ActivitySplashBinding
+import com.github.ybq.android.spinkit.BuildConfig
 
 class Splash : AppCompatActivity() {
+    private val splashTimeOut: Long = 3000
+    private  lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val versionName = "Version " + BuildConfig.VERSION_NAME
+        binding.tvVersion.text = versionName
+
+        Log.d("SplashActivity", "Version Name: $versionName")
+
+        Handler().postDelayed({
+            val intent = Intent(this@Splash, Onboard::class.java)
+            startActivity(intent)
+
+            finish()
+        }, splashTimeOut)
     }
 }
